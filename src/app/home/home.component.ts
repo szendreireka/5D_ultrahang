cf89a3562e837334fea14003a579bdd0b5f4fbcc
szendreireka <szendreireka.74@gmail.com>
2025-04-13 22:06:35 +0200

    projekt

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Adatmodell interfészek
+export interface Patient {
+  name: string;
+  email: string;
+  ultrasoundDate: string;
+}
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  registrationForm: FormGroup;
+  
+  // EventEmitter az adatátadáshoz
+  @Output() formSubmitted: EventEmitter<Patient> = new EventEmitter<Patient>();
 
+  constructor(private fb: FormBuilder) {
+    this.registrationForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      ultrasoundDate: ['', [Validators.required]]
+    });
+  }
+
+  // Form adatküldés
+  onSubmit() {
+    if (this.registrationForm.valid) {
+      const patient: Patient = this.registrationForm.value;
+      this.formSubmitted.emit(patient); // Adat küldése
+    }
+  }
 }
